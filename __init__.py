05a291fac48fff15cd0c964ec3c62485fa2e7e8a
nflamously <david.dembinski.1996@gmail.com>
2024-12-29 23:52:55 +0100

    feat: further refine output for ollama node.

--- __init__.py
+++ __init__.py
@@ -2,16 +2,23 @@ import subprocess
 
 import ollama
 from ollama import Message
-from pydantic import BaseModel
-from transformers.models.udop.convert_udop_to_hf import original_transform
+from pydantic import BaseModel, Field
 
 
-class Prompt(BaseModel):
-    subject: str
-    action: str
-    medium_or_style: str
-    image_quality: str
-    scene: str
+class PositivePrompt(BaseModel):
+    subject: str = Field(max_length=100, min_length=1)
+    medium: str = Field(max_length=20, min_length=1)
+    style: str = Field(max_length=40, min_length=1)
+    art_sharing_website: str = Field(max_length=10, min_length=1)
+    resolution: str = Field(max_length=20, min_length=1)
+    additional_details: str = Field(max_length=80, min_length=1)
+    color: str = Field(max_length=20, min_length=1)
+    lighting: str = Field(max_length=20, min_length=1)
+    composition: str = Field(max_length=40, min_length=1)
+
+class NegativePrompt(BaseModel):
+    negative_quality_tags: str = Field(max_length=175, min_length=1)
+    low_quality_tags: str = Field(max_length=175, min_length=1)
 
 
 def _list_models() -> list:
@@ -61,9 +68,14 @@ class OllamaPromptEnhancer:
         return seed
 
     def _prompt_build(self, original_positive_text, original_negative_text):
-        positive_enhanced_prompt = "enhance the following prompt {} within 200 words in format with multiple words in each category <subject>, <action>, <style and medium>, <image quality>, <scene>. Exclude meta descriptions and just focus on content. Try to include original text from question asked prompt.".format(
+        positive_enhanced_prompt = """
+        Enhance the prompt '{}' by detailing the subject, describe the medium, the style, additional details, the art site the image is from, resolution of image, additional details and tags and color aswell as lighting. Include composition, emotions, poses and a cohesive theme to guide diffusion models in generating a vivid high-quality image.
+        """.format(
             original_positive_text)
-        negative_enhanced_prompt = "generate an enhanced negative prompt from given sentence \"{}\" within 100 words that includes low <image quality>. These tag these words without repeating original phrase. Tagged words must contain following format \"tag1 tag2..\"".format(
+        negative_enhanced_prompt = """
+        Create a concise negative prompt with tags for "{}" of 100 words, focusing on refining image quality.
+        Avoid low resolution, blurry details, pixelation, distortion, grainy textures, overexposure, underexposure, washed-out or dull colors, artifacts, noise, poor lighting, flat composition, lack of depth, unnatural shadows, oversaturation, unbalanced contrast, unrealistic details, amateurish quality, unprofessional finish.
+        """.format(
             original_negative_text)
         return positive_enhanced_prompt, negative_enhanced_prompt
 
@@ -72,10 +84,10 @@ class OllamaPromptEnhancer:
                                    messages=[
                                        Message(
                                            role="user",
-                                           content=positive_input_prompt,
+                                           content="{}".format(positive_input_prompt),
                                        )
                                    ],
-                                   format=Prompt.model_json_schema())
+                                   format=PositivePrompt.model_json_schema())
         negative_res = ollama.chat(model,
                                    messages=[
                                        Message(
@@ -83,7 +95,7 @@ class OllamaPromptEnhancer:
                                            content=negative_input_prompt
                                        )
                                    ],
-                                   format=Prompt.model_json_schema())
+                                   format=NegativePrompt.model_json_schema())
         return positive_res, negative_res
 
     def enhance(self, model, positive_text, negative_text, clip, seed):
@@ -92,16 +104,31 @@ class OllamaPromptEnhancer:
         positive_input_prompt, negative_input_prompt = self._prompt_build(positive_text, negative_text)
         response_positive, response_negative = self._chat(model, positive_text, negative_text, positive_input_prompt, negative_input_prompt)
 
-        positive_prompt_model = Prompt.model_validate_json(response_positive.message.content)
-        positive_prompt = "{} does {} within {} containing {}. Type of image {}".format(positive_prompt_model.subject, positive_prompt_model.action, positive_prompt_model.scene, positive_prompt_model.medium_or_style, positive_prompt_model.image_quality)
-        print("positive prompt received:\n\n{}\n\n".format(positive_prompt))
+        positive_prompt_result = PositivePrompt.model_validate_json(response_positive.message.content)
+        positive_prompt = """
+        Created in {} with a {}, shared on {}. The artwork features {}, {}, {}, {}, and a {}. A {}. 
+        """.format(
+            positive_prompt_result.medium,
+            positive_prompt_result.style,
+            positive_prompt_result.art_sharing_website,
+            positive_prompt_result.resolution,
+            positive_prompt_result.additional_details,
+            positive_prompt_result.color,
+            positive_prompt_result.lighting,
+            positive_prompt_result.composition,
+            positive_prompt_result.subject,
+        )
+        print("positive prompt received:\n{}\n\n".format(positive_prompt))
 
-        negative_prompt_model = Prompt.model_validate_json(response_negative.message.content)
-        negative_prompt = "{} does {} within {} containing {}. Type of image {}".format(negative_prompt_model.subject, negative_prompt_model.action, negative_prompt_model.scene, negative_prompt_model.medium_or_style, negative_prompt_model.image_quality)
-        print("negative prompt received:\n\n{}\n\n".format(negative_prompt))
+        negative_prompt_model = NegativePrompt.model_validate_json(response_negative.message.content)
+        negative_prompt = "{}, {}".format(
+            negative_prompt_model.negative_quality_tags,
+            negative_prompt_model.low_quality_tags,
+        )
+        print("negative prompt received:\n{}\n\n".format(negative_prompt))
 
-        enhanced_output = clip.encode_from_tokens(clip.tokenize(positive_prompt), return_pooled=True, return_dict=True)
-        neg_enhanced_output = clip.encode_from_tokens(clip.tokenize(negative_prompt), return_pooled=True,
+        enhanced_output = clip.encode_from_tokens(clip.tokenize("{}, {}".format(positive_prompt, positive_text)), return_pooled=True, return_dict=True)
+        neg_enhanced_output = clip.encode_from_tokens(clip.tokenize("{}, {}".format(negative_prompt, negative_text)), return_pooled=True,
                                                       return_dict=True)
         output = clip.encode_from_tokens(clip.tokenize(positive_input_prompt), return_pooled=True, return_dict=True)
         return (
---
+++ prompt_diary.md
@@ -0,0 +1,33 @@
+# Example v3
+P: Enhance the prompt '{}' by detailing the subject, their actions, the setting, additional elements, style and medium, image quality, composition, and color harmony. Include specific visual details, emotions, atmospheric lighting, and a cohesive theme to guide diffusion models in generating a vivid and high-quality image.
+N: Create a concise negative prompt with tags for "{}" of 100 words, focusing on refining image quality.
+            Avoid low resolution, blurry details, pixelation, distortion, grainy textures, overexposure, underexposure, washed-out or dull colors, artifacts, noise, poor lighting, flat composition, lack of depth, unnatural shadows, oversaturation, unbalanced contrast, unrealistic details, amateurish quality, unprofessional finish. 
+
+# Example v2
+P:
+            Replace "{}" with a specific prompt and expand it into a detailed sentence or tags using the following structure:
+            * <subject>: A central character or object, a detailed background, complementary elements, natural surroundings, a focal point of interest.
+            * <action>: The subject is engaged in a dynamic or serene activity, interacting with the environment, creating a sense of movement or stillness, evoking emotion.
+            * <style and medium>: Realistic or artistic style, digital painting or photography, inspired by a specific art movement, with balanced composition and harmonious colors. 
+            * <image quality>: High resolution, sharp details, vibrant or muted tones, clean and professional, no distortions or artifacts. 
+            * <scene>: A well-defined setting, with depth and perspective, atmospheric lighting, a cohesive theme, and a visually appealing arrangement of elements.
+            Use only 350 chars at max!
+
+N:
+            Create a concise negative prompt with tags for "{}" of 100 words, focusing on refining image quality.
+            Avoid low resolution, blurry details, pixelation, distortion, grainy textures, overexposure, underexposure, washed-out or dull colors, artifacts, noise, poor lighting, flat composition, lack of depth, unnatural shadows, oversaturation, unbalanced contrast, unrealistic details, amateurish quality, unprofessional finish. 
+      
+
+
+# Example v1
+P:
+            Replace "{}" with a specific prompt and expand it into a detailed sentence or tags using the following structure:
+            * <subject>: A central character or object, a detailed background, complementary elements, natural surroundings, a focal point of interest.
+            * <action>: The subject is engaged in a dynamic or serene activity, interacting with the environment, creating a sense of movement or stillness, evoking emotion.
+            * <style and medium>: Realistic or artistic style, digital painting or photography, inspired by a specific art movement, with balanced composition and harmonious colors. 
+            * <image quality>: High resolution, sharp details, vibrant or muted tones, clean and professional, no distortions or artifacts. 
+            * <scene>: A well-defined setting, with depth and perspective, atmospheric lighting, a cohesive theme, and a visually appealing arrangement of elements.
+            Use only 350 chars at max!
+
+N:
+            create a negative prompt for given sentence \"{}\" within 100 words that includes negative tags for following categories: <image quality>. Do not repeat original prompt.
